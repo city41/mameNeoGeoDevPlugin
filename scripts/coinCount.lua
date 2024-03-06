3bffd92da3b171ba88a01fad176e1e2ae56b4574
Matt Greer <matt.e.greer@gmail.com>
2024-03-06 09:31:01 -0500

    README: remove known issues section

--- scripts/coinCount.lua
+++ scripts/coinCount.lua
@@ -1,9 +1,10 @@
 -- an experimental script that listens to the BIOS setting REG_SETCC1
 -- whenever a coin is entered
 
-REG_SETCC1 = 0x3800E0
+REG_SETCC1 = 0x3800E1
 
 function on_reg_write(offset, data)
+	print(string.format("offset: %x, data: %x", offset, data))
 	if offset == REG_SETCC1 then
 		print("coin entered")
 	end
@@ -12,4 +13,4 @@ end
 cpu = manager.machine.devices[":maincpu"]
 mem = cpu.spaces["program"]
 
-reg_handler = mem:install_write_tap(REG_SETCC1, REG_SETCC1 + 1, "reg", on_reg_write)
+reg_handler = mem:install_write_tap(REG_SETCC1 - 1, REG_SETCC1, "reg", on_reg_write)
